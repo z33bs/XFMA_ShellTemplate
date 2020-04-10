e42fb7d3a0233626742b88c46374e02fa5ff3e75
Zeebs <zeebsmail@gmail.com>
2020-04-10 08:06:08 +0200

    Initial commit

---
+++ XfmaShellTemplate/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XfmaShellTemplate.Models;
+
+namespace XfmaShellTemplate.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
